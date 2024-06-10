1fe530be963c65a5bcdb8dca0b259c3dfe5841cf
04_Apiwat (Tee/ตี๋) <apiwat.pienwisetkaew@gmail.com>
2024-06-10 21:57:05 +0700

    final commit

--- src/components/Owner.jsx
+++ src/components/Owner.jsx
@@ -4,7 +4,7 @@ export default function Owner() {
     return (
         <div className="flex flex-center">
             <h1>Created By</h1>
-            <img src="" alt="Creator" />
+            <img src="../image/Me.JPG" alt="My Picture"/>
             <h1>04_Apiwat(Tee/ตี๋)</h1>
         </div>
     )
